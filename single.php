723048a4e8ab671f1e5ff82e0d0a4797d2a92bea
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2015-06-28 23:40:35 +0200

    Removed post navigation for single-posts.

--- single.php
+++ single.php
@@ -14,11 +14,6 @@ get_header(); ?>
 
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
 			
-			<div class="posts-nav">
-				<h2><?php _e( 'Weitere Beiträge', 'gruene' ); ?></h2>
-				<?php the_post_navigation(); ?>
-			</div><!-- .posts-nav -->
-			
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
 				if ( comments_open() || get_comments_number() ) :
