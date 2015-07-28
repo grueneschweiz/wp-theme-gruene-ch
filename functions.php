ac0a02fadb8c34a231a02ec993bbd44353a6423a
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2015-07-28 21:01:11 +0200

    New version number.

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * Version number of theme. Dont forget to change it also in the style.css file
  */
-define( 'GRUENE_VERSION', '1.6.0' );
+define( 'GRUENE_VERSION', '1.6.1' );
 
 if ( ! function_exists( 'gruene_content_width' ) ) :
 /**
