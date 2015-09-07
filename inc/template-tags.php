cf8d8f088c3564bd28e286553180b89300075dde
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2015-09-07 17:23:44 +0200

    Enhancement: Supports the site icons (fav icon) of the theme customizer now.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -42,6 +42,41 @@ function gruene_language_nav() {
 endif;
 
 
+if ( ! function_exists( 'gruene_the_site_icon_fallback' ) ) :
+/**
+ * echos a default fav-icon, if none is defined in the theme customizer
+ * 
+ * @since 1.8.1
+ */
+function gruene_the_site_icon_fallback() {
+	// get the icon defined in the theme customizer
+	$icon = get_option( 'site_icon' );
+	
+	// if none was defined echo the default ones
+	if ( empty( $icon ) ) {
+		?>
+			<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-icon-57x57.png">
+			<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-icon-60x60.png">
+			<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-icon-72x72.png">
+			<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-icon-76x76.png">
+			<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-icon-114x114.png">
+			<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-icon-120x120.png">
+			<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-icon-144x144.png">
+			<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-icon-152x152.png">
+			<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/apple-icon-180x180.png">
+			<link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/android-icon-192x192.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/favicon-96x96.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/favicon-16x16.png">
+			<link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/manifest.json">
+			<meta name="msapplication-TileColor" content="#ffffff">
+			<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon/ms-icon-144x144.png">
+		<?php
+	}
+}
+endif;
+
+
 if ( ! function_exists( 'gruene_the_post_thumbnail' ) ) :
 /**
  * custom version of the_post_thumbnail() // no attributes supportet!
