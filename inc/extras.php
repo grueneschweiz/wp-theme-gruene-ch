2c31d646489f7337148ff1cdf53669da711d569d
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-01-07 16:27:44 +0100

    Improvement: Only manually add shortcode functionality to text-widget for Wordpress < 4.9

--- inc/extras.php
+++ inc/extras.php
@@ -11,5 +11,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @package Gruene Theme
  */
 
-// Enable short codes for widgets
-add_filter('widget_text','do_shortcode');
+/**
+ * Enable short codes for widgets
+ *
+ * Since WordPress 4.9 this filter is part of the core.
+ */
+if ( version_compare( $GLOBALS['wp_version'], '4.9', '<' ) ) {
+	add_filter('widget_text','do_shortcode');
+}
