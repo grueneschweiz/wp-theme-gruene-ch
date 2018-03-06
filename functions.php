97e0a44a72b126081410f12306725bdf2d0a5eaa
Cyrill Bolliger <cyrill.bolliger@gmail.com>
2018-03-06 09:55:51 +0100

    New version number

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Version number of theme. Dont forget to change it also in the style.css file
  */
-define( 'GRUENE_VERSION', '2.5.1' );
+define( 'GRUENE_VERSION', '2.5.2' );
 
 /**
  * Header image sizes
